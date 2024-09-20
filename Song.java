4372afe845c78db5d4bfef1ceb6439aaec6453c7
2497306 <79730892+2497306@users.noreply.github.com>
2024-09-20 12:52:52 -0700

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,80 @@
+public class Song {
+
+   public static void main(String[] args) {
+
+      paragraph1();
+      paragraph2();
+      paragraph3();
+      paragraph4();
+      paragraph5();
+      paragraph6();
+      
+   }
+   
+
+
+public static void paragraph1(){
+      System.out.println("I once wrote a program that wouldn't compile");
+      System.out.println("I don't know why it wouldn't compile.");
+      System.out.println("My TA just smiled.");
+      
+}
+
+
+public static void paragraph2(){
+ 
+      System.out.println("I searched my brain for all that I knowed,");
+      System.out.println("I <3 the code");
+      System.out.println("I added the println to make things compile,");
+      System.out.println("I don't know why it wouldn't compile.");
+      System.out.println("My TA just smiled.");
+      
+}
+
+
+public static void paragraph3(){
+      System.out.println("I read all the notes I wrote down in class,");
+      System.out.println("Looking for something that might help me pass.");
+      System.out.println("I read the notes to ease my brain,");
+      System.out.println("I added the println to make things compile,");
+      System.out.println("I don't know why it wouldn't compile.");
+      System.out.println("My TA just smiled.");
+
+}
+
+public static void paragraph4(){
+      System.out.println("The info I needed, oh where would it hide?");
+      System.out.println("One of my classmates said 'look at the slides!'");
+      System.out.println("I went through the slides to check my notes");
+      System.out.println("I read the notes to ease my brain,");
+      System.out.println("I added the println to make things compile,");
+      System.out.println("I don't know why it wouldn't compile.");
+      System.out.println("My TA just smiled.");
+
+
+}
+
+
+public static void paragraph5(){
+      System.out.println("I fixed all my syntax, the ; and ");
+      System.out.println("My program, it ran, but stopped with a crash.");
+      System.out.println("I typed everything like it said in the slides");
+      System.out.println("I went through the slides to check my notes");
+      System.out.println("I read the notes to ease my brain,");
+      System.out.println("I added the println to make things compile,");
+      System.out.println("I don't know why it wouldn't compile.");
+      System.out.println("My TA just smiled.");
+
+
+}
+
+
+public static void paragraph6(){
+      System.out.println("With effort and patience I wrote perfect source,");
+      System.out.println("It worked, of course!");
+}
+}
+      
+     
+ 
+
